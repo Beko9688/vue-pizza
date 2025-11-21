dc7a36ee0f1fa022d1f80e2dc75e09febb7d8507
Bekzod Khakimov <bkhakimov@avantstay.com>
2025-11-21 20:23:10 +0300

    add components and views

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,26 @@
-import { fileURLToPath, URL } from 'node:url'
+import {fileURLToPath, URL} from "url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import {defineConfig} from "vite";
+import vue from "@vitejs/plugin-vue";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [vue()],
+    test: {
+        environment: "jsdom",
     },
-  },
-})
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
+    server: {
+        host: true,
+        port: 8080,
+        proxy: {
+            "/api": {
+                target: "http://backend:3000/",
+                rewrite: (path) => path.replace(/^\/api/, ""),
+            },
+        },
+    },
+});
